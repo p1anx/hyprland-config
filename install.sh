8ef2da0738db30e351682cd987d3a28bcf43a930
wsl <2514034568@qq.com>
2024-11-10 02:28:40 +0800

    for install pkg

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo pacman -S --noconfirm neovim
+sudo pacman -S --noconfirm waybar
+sudo pacman -S --noconfirm rofi-wayland
+sudo pacman -S --noconfirm wallust
+sudo pacman -S --noconfirm fzf
+sudo pacman -S --noconfirm wlogout
+sudo pacman -S --noconfirm tmux
+sudo pacman -S --noconfirm dunst
+sudo pacman -S --noconfirm swaync
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
+# sudo pacman -S --noconfirm neovim
