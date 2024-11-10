39a95ce976690ecfc9918fe81b2164cf23c44eb1
dorm arch <2514034568@qq.com>
2024-11-10 21:17:44 +0800

    update

--- config/hypr/initial-boot.sh
+++ config/hypr/initial-boot.sh
@@ -11,6 +11,7 @@
 scriptsDir=$HOME/.config/hypr/scripts
 wallpaper=$HOME/.config/hypr/wallpaper_effects/.wallpaper_modified
 waybar_style="$HOME/.config/waybar/style/[Dark] Latte-Wallust combined.css"
+# waybar_style="$HOME/.config/waybar/style/myTheme0.css"
 kvantum_theme="Catppuccin-Mocha"
 color_scheme="prefer-dark"
 gtk_theme="Andromeda-dark"
